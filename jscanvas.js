c2c36bf7138d6e616308ca8907485f2d68b8b960
thang nguyen <nguyendinhducthang95@gmail.com>
2023-01-22 23:57:02 +0700

    frist commit thang nguyen

---
+++ jscanvas.js
@@ -0,0 +1,5 @@
+var c=document.getElementById('mycanvas');
+var ctx=c.getContext('2d');
+ctx.beginPath();
+ctx.arc(200,100,50,0,1*Math.PI);
+ctx.stroke();
---
+++ jsmain.js
@@ -0,0 +1,18 @@
+/*
+Kiểu dữ liệu trong javascript
+1.Dữ liệu nguyên thủy
+- number
+- string
+- null
+- boolean
+- undefine
+- symbol
+
+*/
+var a=1;
+var b='1';
+console.log(re)
+re=null||undefined||false||'D';
+
+
+
